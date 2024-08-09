33d8cf49205b2115c4552c0f1d74d2ed7d69ea20
Oliver <olivercreativeworks@gmail.com>
2024-08-09 08:01:17 -0400

    add example code back to jsdoc descriptions

--- main.js
+++ main.js
@@ -37,6 +37,20 @@ function write(message){
  * 
  * This method will write as many pending messages as possible to the log and set up a trigger to write additional messages if need be.
  * 
+ * #### Commit to the log (3 steps)
+ * ``` 
+ * // 1) define the log sheet
+ * const logSheet = SpreadsheetApp.openById(YOUR-SPREADSHEET-ID).getSheetByName(YOUR-LOG-SHEET-NAME)
+ * 
+ * // 2) define a function to write pending messages to the log on a clock trigger
+ * function writePendingMessages(e){
+ *    SpreadsheetLogger.writePendingMessagesToLog(e, logSheet, LockService.getScriptLock(), writePendingMessages.name)
+ * }
+ * 
+ * // 3) call commit to actually append the messages to the log
+ * logger.commitToLog(logSheet, LockService.getScriptLock(), writePendingMessages.name) 
+ * ``` 
+ * 
  * @param {SpreadsheetApp.Sheet} logSheet The first column of the log sheet should be for timestamps, the second column is for a message. The written timestamps and messages will be appended to the end of the sheet. 
  * @param {LockService.Lock} userProvidedLock
  * @param {string} processPendingMessages The name of the function that will be responsible for appending pending messages to the log. This function should call the method writePendingMessagesToLog and pass in the clock event trigger. See the writePendingMessagesToLog method for more details.
@@ -50,6 +64,20 @@ function commitToLog(logSheet, userProvidedLock, processPendingMessages){
  * Appends messages from the pending message queue onto the spreadsheet log. To get pending messages, pass in the same sheet you used in the commitToLog function.
  * 
  * This method will write as many pending messages as possible to the log and set up a trigger to write additional messages if need be.
+ * 
+ * #### Commit to the log (3 steps)
+ * ``` 
+ * // 1) define the log sheet
+ * const logSheet = SpreadsheetApp.openById(YOUR-SPREADSHEET-ID).getSheetByName(YOUR-LOG-SHEET-NAME)
+ * 
+ * // 2) define a function to write pending messages to the log on a clock trigger
+ * function writePendingMessages(e){
+ *    SpreadsheetLogger.writePendingMessagesToLog(e, logSheet, LockService.getScriptLock(), writePendingMessages.name)
+ * }
+ * 
+ * // 3) call commit to actually append the messages to the log
+ * logger.commitToLog(logSheet, LockService.getScriptLock(), writePendingMessages.name) 
+ * ``` 
  * 
  * @param {object} e Clock trigger event object
  * @param {string} e.triggerUid The unique id for the trigger
