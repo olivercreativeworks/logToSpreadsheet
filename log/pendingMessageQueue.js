abaac90532a56c72480f2d924c9372f4990b7dbf
Oliver <olivercreativeworks@gmail.com>
2024-08-08 20:31:26 -0400

    added id parameter and removed the lock from pending queue

--- log/pendingMessageQueue.js
+++ log/pendingMessageQueue.js
@@ -4,7 +4,6 @@
 function getPendingMessagesQueue_(){
   const props = PropertiesService.getScriptProperties()
   const pendingMessageSymbol = 'messageKey'
-  const lockManager = getLockManager_()
   return {
     getMessagesOut: getMessagesOutOfQueue,
     addMessages: addToQueue,
@@ -14,28 +13,36 @@ function getPendingMessagesQueue_(){
    * Returns currently queued messages and clears the queue. Returns null if no messages are found. 
    * 
    * **Uses a user lock to prevent concurrent reads and writes** 
+   * @param {string} id
    */
-  function getMessagesOutOfQueue(){
-    return lockManager.handleUserLock(() => {
-      try{
-        return getMessages()
-      }finally{
-        clearQueue()
-      }
-    })
+  function getMessagesOutOfQueue(id){
+    try{
+      return getMessages(id)
+    }finally{
+      clearQueue(id)
+    }
   }
 
   /** 
-   * Returns null if no messages are pending. 
+   * Returns null if no messages are pending.
+   * @param {string} id
    * @return {LogMessage[] | null} 
    */
-  function getMessages(){
+  function getMessages(id){
     console.log('Retrieving pending messsages.')
-    return JSON.parse(props.getProperty(pendingMessageSymbol))
+    return JSON.parse(props.getProperty(makePendingMessageSymbol(id)))
   }
   
-  function clearQueue(){
-    props.deleteProperty(pendingMessageSymbol)
+  /**
+   * @param {string} id
+   */
+  function makePendingMessageSymbol(id){
+    return pendingMessageSymbol + id
+  }
+
+  /** @param {string} id */
+  function clearQueue(id){
+    props.deleteProperty(makePendingMessageSymbol(id))
     console.log('Deleted pending messages')
   }
 
@@ -44,14 +51,12 @@ function getPendingMessagesQueue_(){
    * 
    * **Uses a user lock to prevent concurrent reads and writes**
    * @param {LogMessage[]} messages
+   * @param {string} id The id messages should be stored under.
    */
-  function addToQueue(messages){
-    console.warn('Adding message to pending message queue...')
-    lockManager.handleUserLock(() => {
-      console.log(messages)
-      const savedMessages = getMessages() || []
-      props.setProperty(pendingMessageSymbol, JSON.stringify([...savedMessages, ...messages]))
-      console.log('Added message to queue')
-    })
+  function addToQueue(messages, id){
+    console.warn(`Adding message to pending message queue...:\n${messages}`)
+    const savedMessages = getMessages() || []
+    props.setProperty(makePendingMessageSymbol(id), JSON.stringify([...savedMessages, ...messages]))
+    console.log('Added message to queue')
   }
 }
