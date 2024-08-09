9446c86bb994018475a32b42644a801b477adde4
Oliver <olivercreativeworks@gmail.com>
2024-08-09 07:49:26 -0400

    update jsdoc description

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const stagingArea = getStagingArea_()
+
 /** 
  * You can write any number of messages to the log before calling commit.
  * 
@@ -18,13 +19,13 @@ const stagingArea = getStagingArea_()
  * // 1) define the log sheet
  * const logSheet = SpreadsheetApp.openById(YOUR-SPREADSHEET-ID).getSheetByName(YOUR-LOG-SHEET-NAME)
  * 
- * // 2) define a function to write pending messages to the log
+ * // 2) define a function to write pending messages to the log on a clock trigger
  * function writePendingMessages(e){
  *    SpreadsheetLogger.writePendingMessagesToLog(e, logSheet, LockService.getScriptLock(), writePendingMessages.name)
  * }
  * 
  * // 3) call commit to actually append the messages to the log
- * logger.commitToLog(logSheet, writePendingMessages.name) 
+ * logger.commitToLog(logSheet, LockService.getScriptLock(), writePendingMessages.name) 
  * ``` 
  * @param {string} message 
  */
@@ -32,20 +33,10 @@ function write(message){
   stagingArea.stageMessage(message)
 }
 /** 
- * Call this after writing some messages
- * #### Commit to the log (3 steps)
- * ``` 
- * // 1) define the log sheet
- * const logSheet = SpreadsheetApp.openById(YOUR-SPREADSHEET-ID).getSheetByName(YOUR-LOG-SHEET-NAME)
+ * Call this after writing some messages. 
  * 
- * // 2) define a function to write pending messages to the log
- * function writePendingMessages(e){
- *    SpreadsheetLogger.writePendingMessagesToLog(e, logSheet, LockService.getScriptLock(), writePendingMessages.name)
- * }
+ * This method will write as many pending messages as possible to the log and set up a trigger to write additional messages if need be.
  * 
- * // 3) call commit to actually append the messages to the log
- * logger.commitToLog(logSheet, writePendingMessages.name) 
- * ``` 
  * @param {SpreadsheetApp.Sheet} logSheet The first column of the log sheet should be for timestamps, the second column is for a message. The written timestamps and messages will be appended to the end of the sheet. 
  * @param {LockService.Lock} userProvidedLock
  * @param {string} processPendingMessages The name of the function that will be responsible for appending pending messages to the log. This function should call the method writePendingMessagesToLog and pass in the clock event trigger. See the writePendingMessagesToLog method for more details.
@@ -56,18 +47,14 @@ function commitToLog(logSheet, userProvidedLock, processPendingMessages){
 }
 
 /**
- * Appends messages from the pending message onto the spreadsheet log saved in the properties service of this script.
- * #### Write pending messages to the log
- * ``` 
- * // commit stored pending messages
- * function processPendingMessages(e){
- *    SpreadsheetLogger.writePendingMessagesToLog(e, logSheet, LockService.getScriptLock(), writePendingMessages.name)
- * }
- * ``` 
+ * Appends messages from the pending message queue onto the spreadsheet log. To get pending messages, pass in the same sheet you used in the commitToLog function.
+ * 
+ * This method will write as many pending messages as possible to the log and set up a trigger to write additional messages if need be.
+ * 
  * @param {object} e Clock trigger event object
  * @param {string} e.triggerUid The unique id for the trigger
- * @param {SpreadsheetApp.Sheet} logSheet The first column of the log sheet should be for timestamps, the second column is for a message. The written timestamps and messages will be appended to the end of the sheet. 
- * @param {LockService.Lock} userProvidedLock
+ * @param {SpreadsheetApp.Sheet} logSheet Should match the sheet you used in commitToLog. Pending messages will be appended to the end of the sheet. 
+ * @param {LockService.Lock} userProvidedLock A lock that will be used to prevent concurrent writes to the sheet.
  * @param {string} processPendingMessages The name of the function that will be responsible for appending pending messages to the log. The function should call this method inside it's body.
  */
 function writePendingMessagesToLog(e, logSheet, userProvidedLock, processPendingMessages){
